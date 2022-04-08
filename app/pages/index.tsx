cc216baf624c4a5a7df52950e53478a448618359
Jurredr <github@jurre.me>
2022-04-08 21:05:40 +0200

    Finished hero of landing page

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -6,7 +6,7 @@ const Home: BlitzPage = () => {
   return (
     <div>
       <div
-        className="h-screen w-screen"
+        className="h-screen w-screen relative"
         style={{
           background:
             'linear-gradient(120deg, rgba(255,172,213,1) 0%, rgba(254,250,166,1) 65%, rgba(255,253,210,1) 100%)'
@@ -24,38 +24,62 @@ const Home: BlitzPage = () => {
                 alt="Logo icon of smiling face"
               />
             </div>
-            <div className="flex justify-center items-center">
-              <Link href="/sign-in" scroll={false} passHref>
+            <div className="flex justify-center items-center gap-6 font-medium tracking-tight">
+              <Link href="/" scroll={false} passHref>
                 <div className="flex justify-center items-center gap-2 transition-all hover:gap-[0.35rem] hover:ml-[0.15rem] cursor-pointer whitespace-nowrap">
-                  <FiLogIn />
-                  <p>Sign in</p>
+                  <FiLogIn strokeWidth={3} />
+                  <p className="noselect text-lg">Sign in</p>
                 </div>
               </Link>
-              <Link href="/sign-up" scroll={false} passHref>
-                <p>Sign Up</p>
+              <Link href="/" scroll={false} passHref>
+                <div className="relative cursor-pointer">
+                  <div className="flex active:top-[0.15rem] justify-center z-10 items-center relative bg-white rounded-2xl h-[3rem] w-[9rem] border-[0.2rem] border-black">
+                    <p className="noselect text-lg">Sign up</p>
+                  </div>
+                  <div className="absolute h-[3rem] w-[9rem] rounded-2xl bg-black -bottom-1" />
+                </div>
               </Link>
             </div>
           </div>
           <div className="flex justify-between">
-            <div>
+            <div className="flex flex-col gap-12">
               <div className="font-semibold text-white text-[5rem]">
-                <h1 className="leading-tight">
+                <h1 className="leading-tight mt-40">
                   One Card,<br></br>All your Socials.
                 </h1>
               </div>
-              <div>socials.lol/@yourname</div>
+              <div className="relative cursor-text">
+                <div className="flex justify-between pl-7 pr-2 z-10 items-center relative bg-white rounded-full h-[4rem] w-full border-[0.2rem] border-black">
+                  <p className="noselect text-[1.8rem] tracking-tight font-medium">
+                    socials.lol/@<span className="text-gray-400 opacity-80">yourname</span>
+                  </p>
+                  <button className="noselect tracking-tight text-[1.4rem] font-medium rounded-full bg-gray-100 px-4 py-[0.3rem]">
+                    Sign up
+                  </button>
+                </div>
+                <div className="absolute h-[4rem] w-full rounded-full bg-black -bottom-2" />
+              </div>
             </div>
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
-              className="h-[30rem] pr-16"
+              className="h-[30rem] pr-20 mt-32"
               draggable={false}
               src="/landing-hero-phone.png"
               alt="Landing hero phone display"
             />
           </div>
         </div>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          className="h-[17rem] absolute -bottom-[5.5rem] left-[20rem]"
+          draggable={false}
+          src="/arrow-bubble.svg"
+          alt="Arrow pointing downwards"
+        />
       </div>
-      <div className="h-screen"></div>
+      <div className="h-screen flex justify-center pt-40">
+        <h1 className="font-semibold text-[5rem]">Share everything.</h1>
+      </div>
       <div className="bg-black w-screen h-[7.5rem] flex px-44 justify-between items-center -z-20">
         <div className="flex justify-center items-center gap-3 cursor-pointer noselect">
           <p className="font-newkansas font-semibold text-white text-[2.7rem]">socials.lol</p>
