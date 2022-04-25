ca39ea55217a619dcf4005734f196c4d045b7659
Jurredr <github@jurre.me>
2022-04-25 17:06:39 +0200

    Added TOS and Privacy Statement base pages

---
+++ pages/privacy-statement.tsx
@@ -0,0 +1,23 @@
+import { NextPage } from 'next'
+import Footer from '../components/Footer'
+import LandingNav from '../components/LandingNav'
+
+const PrivacyStatement: NextPage = () => {
+  return (
+    <div className="h-screen w-screen relative flex flex-col items-center justify-between">
+      <div
+        className="w-screen px-6 sm:px-10 xl:px-44 py-3"
+        style={{
+          background:
+            'linear-gradient(120deg, rgba(255,172,213,1) 0%, rgba(254,250,166,1) 65%, rgba(255,253,210,1) 100%)'
+        }}
+      >
+        <LandingNav />
+      </div>
+      <h1 className="font-semibold text-6xl">Privacy Statement</h1>
+      <Footer />
+    </div>
+  )
+}
+
+export default PrivacyStatement
