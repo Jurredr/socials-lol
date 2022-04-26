8f724261d42068be6706bc27053ba04cc2418aa1
Jurredr <github@jurre.me>
2022-04-26 17:46:15 +0200

    Added fancy page loading progress bar at the top of the screen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import { SessionProvider } from 'next-auth/react'
 import { AppProps } from 'next/app'
+import { Router } from 'next/router'
 import WebHead from '../components/WebHead'
+import NProgress from 'nprogress'
 import '../styles/globals.scss'
+import '../styles/pageprogress.scss'
+
+// Set up NProgress
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
+NProgress.configure({ showSpinner: false })
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
